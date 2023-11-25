5787964c60cc856d11f36a175a4082fea5d20fb0
Aleksandra Zięba <aleksandrazieba@Aleksandras-MacBook-Pro.local>
2023-11-25 10:22:14 +0100

    Assignments 1-3

---
+++ build/src/Application/CMakeFiles/application.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/aleksandrazieba/Documents/grafika/3d/src/Application/application.cpp" "src/Application/CMakeFiles/application.dir/application.cpp.o" "gcc" "src/Application/CMakeFiles/application.dir/application.cpp.o.d"
+  "/Users/aleksandrazieba/Documents/grafika/3d/src/Application/shader_source.cpp" "src/Application/CMakeFiles/application.dir/shader_source.cpp.o" "gcc" "src/Application/CMakeFiles/application.dir/shader_source.cpp.o.d"
+  "/Users/aleksandrazieba/Documents/grafika/3d/src/Application/utils.cpp" "src/Application/CMakeFiles/application.dir/utils.cpp.o" "gcc" "src/Application/CMakeFiles/application.dir/utils.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
